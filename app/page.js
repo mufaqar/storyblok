3fe258b693f43b95f2125daf1c222be74fee5e7d
mufaqar <mufaqar@gmail.com>
2023-08-31 13:43:10 +0500

    storyblok

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import {
+  getStoryblokApi,
+} from "@storyblok/react/rsc";
+import StoryblokStory from "@storyblok/react/story";
+
+export default async function Home() {
+  const { data } = await fetchData();
+
+  return (
+    <div>
+      <StoryblokStory story={data.story} />
+    </div>
+  );
+}
+
+export async function fetchData() {
+  let sbParams = { version: "published" };
+
+  const storyblokApi = getStoryblokApi();
+  return storyblokApi.get(`cdn/stories/home`, sbParams);
+}
